547c59aa25522aec630140628571da30df554870
mathewabin8024-bit <mathewabin8024@gmail.com>
2025-10-04 12:23:30 +0530

    Add files via upload

---
+++ python lab/arith_f.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age:"))
+
+if age<10:
+     print("ticket rate is =7")
+if age>=10 and age<60:
+    print("ticket rate is =10")
+if age>=60:
+     print("ticket rate is =5")
